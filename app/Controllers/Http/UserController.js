9d7fbf0f735c5be8ae989f84568e802655b94871
alefwhite <alefwhite@hotmail.com>
2021-07-05 23:38:19 -0300

    Finalizando

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,30 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
-  async store({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+  async store({ request, response }) {
+    const trx = await Database.beginTransaction()
 
-    const user = await User.create(data)
+    try {
 
-    return user
+      const data = request.only(['username', 'email', 'password'])
+      const addresses = request.input('addresses')
+
+      const user = await User.create(data, trx)
+
+      await user.addresses().createMany(addresses, trx)
+
+      await trx.commit()
+
+      return user
+
+    } catch (err) {
+        await trx.rollback()
+        return response.status(err.status).send({ error: { message: error.messages } })
+    }
+
   }
 }
 
